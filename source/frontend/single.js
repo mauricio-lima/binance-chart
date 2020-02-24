db742ca262bd1e16adc3763677586324b58ab217
Mauricio Lima <github@mauriciolima.eti.br>
2020-02-24 19:10:28 -0300

    Bootstrap Date picker for select date of chart

--- source/frontend/single.js
+++ source/frontend/single.js
@@ -1,8 +1,25 @@
 (()=>{
     async function DOMLoaded()
     {
-        const filename = 'btc-usdt//2020//02//data-btcusdt-20200223-0000-20200223-2359-utc'
+        $('.datepicker')
+            .on('changeDate', async (e) => {
+                $(e.target).datepicker('hide')
 
+                const date = `${e.date.getUTCFullYear()}${('0' + (e.date.getUTCMonth() + 1)).substr(-2,2)}${('0' + (e.date.getUTCDate())).substr(-2,2)}`
+                const filename = `btc-usdt//${date.substr(0,4)}//${date.substr(4,2)}//data-btcusdt-${date}-0000-${date}-2359-utc`
+
+                console.log(filename)
+                const data = await loadJSON('data//' + filename + '.json')
+                setupCandleStickChart(document.getElementById("container-1"), data , 'candles')
+            })
+            .datepicker({
+                format    : 'dd/m/yyyy',
+                language  : 'pt-BR',
+                weekStart :  1,
+                color     : 'red'
+            })
+
+        const filename = 'btc-usdt//2020//02//data-btcusdt-20200223-0000-20200223-2359-utc'
         setupCandleStickChart(document.getElementById("container-1"), await loadJSON('data//' + filename + '.json'), 'candles')
     }
 
@@ -22,10 +39,10 @@
         }
         catch (e)
         {
-            debugger
-            console.log('fetch fail')
+            throw e
         }
     }
 
-    document.addEventListener('DOMContentLoaded', DOMLoaded)
+    $( document ).ready(DOMLoaded)
+    //document.addEventListener('DOMContentLoaded', DOMLoaded)
 })()
